8320c93478035907c20fa9e63dc3760b9402363f
cammelCase <jack.kettle2@mail.dcu.ie>
2015-02-24 00:14:02 +0000

    Fixing data

--- src/clubCsvFromJson/Main.java
+++ src/clubCsvFromJson/Main.java
@@ -77,9 +77,9 @@ public class Main {
 				+ "Dmt Level,"
 				+ "Trampoline Form Judge,"
 				+ "Trampoline Tariff Judge,"
-				+ "Trampoline Sync Competitor,"
+				+ "Trampoline Sync Judge,"
 				+ "Trampoline Superior Judge,"
-				+ "Tumbling JudgeLevel,"
+				+ "Trampoline Judge Level,"
 				+ "Tumbling Judge,"
 				+ "Tumbling JudgeLevel,"
 				+ "Tumbling Superior Judge,"
@@ -98,100 +98,130 @@ public class Main {
 				if(c.getMembers() != null){
 					System.out.println("Building info for " + c.getName());
 				    for(Member m: c.getMembers()){
+				    	// Club name
 				    	writer.append(c.getName() + ",");
-					    writer.append(m.getName() + ",");
+					    // Name
+				    	writer.append(m.getName() + ",");
+					    // Gender
 					    writer.append(m.getGender() + ",");
+					    // Shirt
 					    if(m.isShirt()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+					    // Shirt Size
 					    writer.append(m.getShirtSize() + ",");
+					    // Shirt Color
 					    writer.append(m.getShirtColor() + ",");
+					    // Past commisto
 					    if(m.isPastCommISTO()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+					    // Social
 					    if(m.isSocialTicket()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+					    // Trampoline
 					    if(m.isTrampolineCompetitor()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+					    // Trampoline sync
 					    if(m.isTrampolineSyncCompetitor()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+					    // Trampoline sync partner
 					    writer.append(m.getTrampolineSyncPartner() + ",");
+					    // Trampoline Level
 					    writer.append(m.getTrampolineLevel() + ",");
+					    // Trampoline Sync Level
 					    writer.append(m.getTrampolineSyncLevel() + ",");
+					    // trampoline team
 					    writer.append(m.getTrampolineTeam() + ",");
-				    	if(m.isTumblingCompetitor()){
+					    // tumbling levelCompetitor
+					    if(m.isTumblingCompetitor()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// tumbling level
 				    	writer.append(m.getTumblingLevel() + ",");
+				    	// dmt competitor
 				    	if(m.isDmtCompetitor()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// dmt level
 				    	writer.append(m.getDmtLevel() + ",");
+				    	// Trampoline form Judge
 				    	if(m.isTrampolineFormJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// Trampoline tariff Judge
 				    	if(m.isTrampolineTariffJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
-					    }	
-				    	if(m.isTrampolineSyncCompetitor()){
+					    }
+				    	// Trampoline Sync Judge
+				    	if(m.isTrampolineSyncJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// Trampoline Superior Judge
 				    	if(m.isTrampolineSuperiorJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
-				    	writer.append(m.getTumblingJudgeLevel() + ",");
+				    	// Trampoline Judge Level
+				    	writer.append(m.getTrampolineJudgeLevel() + ",");
+				    	// Tumbling Judge
 				    	if(m.isTumblingJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// Tumbling JudgeLevel
 				    	writer.append(m.getTumblingJudgeLevel() + ",");
+				    	// Tumbling Superior Judge
 				    	if(m.isTumblingSuperiorJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// DmtJudge
 				    	if(m.isDmtJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
 				    	writer.append(m.getDmtJudgeLevel() + ",");
+				    	// DmtSuperiorJudge
 				    	if(m.isDmtSuperiorJudge()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// Scorekeeping
 				    	if(m.isScorekeeper()){
 					    	writer.append(YES + ",");
 					    }else{
 					    	writer.append(NO + ",");
 					    }
+				    	// Marshalling
 				    	if(m.isMarshling()){
 					    	writer.append(YES);
 					    }else{
